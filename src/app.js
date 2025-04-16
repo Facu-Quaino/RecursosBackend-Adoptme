99a233727bfc48bd8e8d6d23637b3d3e345c0418
Facu-Quaino <facuquaino24@gmail.com>
2025-04-16 03:11:47 -0300

    added swagger

--- src/app.js
+++ src/app.js
@@ -6,18 +6,26 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
-import { engine } from 'express-handlebars';
-import {__dirname} from "./utils/index.js"
-import path from "path"
-import { resolve } from 'path';
 import viewsRouter from "./routes/views.router.js"
+import { faker } from '@faker-js/faker';
+import errorHandler from './middlewares/errors.js'
+import { addLogger, logger } from './utils/logger.js';
+import { swaggerOptions } from './utils/swagger.js';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors'
+import { engine } from 'express-handlebars';
+import path from "path"
+import { baseName } from './utils/index.js';
+
 
 dotenv.config()
-const baseName = resolve(__dirname, "../")
 
 const app = express();
 const PORT = process.env.PORT||8080;
 const connection = mongoose.connect(process.env.MONGO_URL)
+
+app.use(cors())
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,10 +39,46 @@ app.engine("hbs", engine({extname: ".hbs",
 app.set("view engine", "hbs")
 app.set("views", path.join(baseName, "views"))
 
+app.use(addLogger)
+const specs = swaggerJsdoc(swaggerOptions)
+app.use("/docs",swaggerUi.serve,swaggerUi.setup(specs))
+
 app.use("/", viewsRouter)
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.get('/api/test/user', (req, res) => {
+    let first_name = faker.person.firstName();
+    let last_name = faker.person.lastName();
+    let email = faker.internet.email();
+    let password = faker.internet.password();
+    res.send({ first_name, last_name, email, password });
+});
+
+app.get("/loggertest",(req,res)=>{
+    //req.logger.info("Info message")
+    req.logger.error("Error message")
+    req.logger.warn("Warning message")
+    req.logger.debug("Debug message")
+    req.logger.fatal("Fatal message")
+    res.send("Logger test")
+})
+
+app.get("/mockingpets",(req,res)=> {
+    const pets = []
+    const speciesList = ["dog","cat","fish"]
+
+    for(let i=0;i<100;i++){
+        pets.push({
+            name:faker.person.firstName(),
+            specie:faker.helpers.arrayElement(speciesList),
+            birthDate:faker.date.birthdate({min:1,max:15,mode:"age"}).toISOString().split("T")[0]
+        })
+    }
+    res.send({status:"success",payload:pets})
+})
+
+app.use(errorHandler)
+app.listen(PORT,()=>logger.info(`Listening on ${PORT}`))
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import {fileURLToPath} from 'url';
-import { dirname } from 'path';
+import { dirname, resolve } from 'path';
 
 export const createHash = async(password) =>{
     const salts = await bcrypt.genSalt(10);
@@ -10,6 +10,7 @@ export const createHash = async(password) =>{
 export const passwordValidation = async(user,password) => bcrypt.compare(password,user.password);
 
 const __filename = fileURLToPath(import.meta.url);
-export const __dirname = dirname(__filename);
+const __dirname = dirname(__filename);
+export const baseName = resolve(__dirname, "../")
 
 export default __dirname;
